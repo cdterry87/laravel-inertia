199a57bdc094f01db56c0c0eef12f3ecc4d424ea
Chase Terry <cterry@lightbeacon.com>
2021-10-27 14:48:22 -0500

    Created Laravel app, installed Inertia for Laravel and Vue, and created the first Vue page and passing data via the route

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Hello, {{ name }}!</h1>
+    <p>In this course, we will use the following frameworks:</p>
+    <ul>
+      <li v-for="(framework, index) in frameworks" :key="index">
+        {{ framework }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      frameworks: {
+        type: Array
+      }
+    }
+  }
+</script>
